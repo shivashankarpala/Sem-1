5181ae0dfec33dc1b3dcaf478422fe58c4dd5d54
shivashankarpala <155003499+shivashankarpala@users.noreply.github.com>
2024-11-10 13:57:46 +0530

    Update total-energy.c

--- cprog-lab/total-energy.c
+++ cprog-lab/total-energy.c
@@ -1,1 +1,17 @@
 #include<stdio.h>
+
+void main()
+{
+  float m, v, h, ke, pe, te;
+
+  printf("Input Following: Mass, Velocity, Height\n");
+  scanf("%f%f%f", &m, &v, &h);
+
+  ke = (0.5) * m * v * v;
+  pe = m * 9.8 * h;
+  te = ke + pe;
+
+  printf("\nKinetic Energy= %f J", ke);
+  printf("\nPotential Energy= %f J", pe);
+  printf("\nTotal Energy= %f J", te);
+}
