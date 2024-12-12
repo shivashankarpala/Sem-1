4141c0f665821f839337dbc31b716b7733954e3f
shivashankarpala <shivashankarpala7@gmail.com>
2024-12-12 20:18:13 +0530

    new files

--- cprog/lab/total-energy.c
+++ cprog/lab/total-energy.c
@@ -4,8 +4,12 @@ void main()
 {
   float m, v, h, ke, pe, te;
 
-  printf("Input Following: Mass, Velocity, Height\n");
-  scanf("%f%f%f", &m, &v, &h);
+  printf("\nInput the Mass in kg : ");
+  scanf("%f", &m);
+  printf("\nInput the Velocity in m/s : ");
+  scanf("%f",&v);
+  printf("\nInput the Height in m : ");
+  scanf("%f",&h);
 
   ke = (0.5) * m * v * v;
   pe = m * 9.8 * h;
